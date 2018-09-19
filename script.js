8d019d59438252214cc7c29ab48b9c0e01a72789
alexyancey <james.alexander.yancey@gmail.com>
2018-09-19 16:22:59 +0000

    added ids

--- script.js
+++ script.js
@@ -6,8 +6,43 @@ var responses = ["The Password and Confirm Password must match.",
                 "It has been ever so fun to troll you."]
 
 
-function checkIfEmpty(){
+function reset()
+{
+    document.getElementById("txtFirstName").value = "";
+    //document.getElementById("errFirstName").visibility = "hidden";
     
+    //document.getElementsByName("txtMiddleName").value = "";
+}
+
+function checkIfEmpty(){
+    var firstName = document.getElementById("txtFirstName").value;
+    var middleName = document.getElementById("txtMiddleName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    var address = document.getElementById("txtAddress").value;
+    var city = document.getElementById("txtCity").value;
+    var state = document.getElementById("txtState").value;
+    var email = document.getElementById("txtEmail").value;
+    var password = document.getElementById("txtPassword").value;
+    var confirm = document.getElementById("txtConfirm").value;
+    
+    if (firstName === "")
+        document.getElementById("errFirstName").style.visibility = "visible";
+    if (middleName === "")
+        document.getElementById("errMiddleName").style.visibility = "visible";
+    if (lastName === "")
+        document.getElementById("errLastName").style.visibility = "visible";
+    if (address === "")
+        document.getElementById("errAddress").style.visibility = "visible";
+    if (city === "")
+        document.getElementById("errCity").style.visibility = "visible";
+    if (state === "")
+        document.getElementById("errCity").style.visibility = "visible";
+    if (email === "")
+        document.getElementById("errEmail").style.visibility = "visible";
+    if (password === "")
+        document.getElementById("errPassword").style.visibility = "visible";
+    if (confirm === "")
+        document.getElementById("errConfirm").style.visibility = "visible";
 }
 
 function checkIfSame(){
@@ -18,5 +53,10 @@ function checkIfSame(){
     }else{
         
     }
+}
+
+function submit()
+{
+    reset();
 }
 
